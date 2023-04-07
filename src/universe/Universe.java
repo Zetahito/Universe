e1123726ab1056950b647679b9dfa815d502c204
Horn-of-plenty <Jakub.Stych@email.cz>
2023-04-07 12:11:03 +0200

    The Big Bang (7.4.2023)

--- src/universe/Universe.java
+++ src/universe/Universe.java
@@ -1,5 +1,16 @@
 package universe;
 
+/**
+ * <h1>Vesmír</h1>
+ * Obsahuje veškeré astronomické objekty.
+ * @author Jakub Štych
+ * @version 7.4.2023
+ */
 public abstract class Universe {
-    boolean existInUniverse = true;
+
+    /**
+     * <h3>Existence</h3>
+     * Potomci této třídy budou mít existenci nastavenou na <u>true</u>
+     */
+    final boolean existing = true;
 }
